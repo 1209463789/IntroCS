97616e67bafd313abf602ace8ccb9585e342a0a8
1209463789 <18ayuye@iaa.edu.ec>
2015-05-07 21:12:48 -0500

    Create HelloWorld.py

---
+++ HelloWorld.py
@@ -0,0 +1,27 @@
+print "HelloWorld"
+print "My name is Arlen"
+print "I am 14 years old \nThis is Python"
+
+print 2015 + 1 #Printing an interger
+print "2015"
+
+print 3.1416 #Printing a floating print number
+
+print 2018 + 7 #adding a number
+
+print 2018 - 2 #substracting a number
+
+print 2018* 5 #Multiplying a number
+
+#Math Operations
+print 1+1
+print 1-1
+print 1*1
+print 1/1-1
+
+#Modulus
+print 10%3
+
+print "Odd or Even Numbers"
+print 17 % 2
+print 16 % 2018
