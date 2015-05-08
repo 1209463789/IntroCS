8d2763fed5a14731825652fce5826604217ddfa2
1209463789 <18ayuye@iaa.edu.ec>
2015-05-07 21:13:36 -0500

    Create Classwork.py

---
+++ Classwork.py
@@ -0,0 +1,17 @@
+mystring = "Hello" #string 
+myfloat = 10 #floatingpoint
+myint = 20 #interger
+print
+#Printing on Screen
+print mystring 
+print myfloat
+print myint
+print 
+print mystring, ",My cousin is ", myfloat, "years old", ",he will be", myint, "in ten years"
+print 
+#Mathematical operations
+print 5*3/3
+print (6+12)*2
+print 60/10*2
+print 8+8-2
+print 9*9*3
