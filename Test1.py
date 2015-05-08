0eba3c1c065654b3ee5393d29745efbb87fea8ca
1209463789 <18ayuye@iaa.edu.ec>
2015-05-07 21:16:24 -0500

    Create Test1.py

---
+++ Test1.py
@@ -0,0 +1,11 @@
+operand1 = 95
+operand2 = 64.5 
+#operations
+print operand1 + operand2
+print operand1 - operand2
+print operand1 * operand2
+print operand1 / operand2
+print operand1 % operand2
+
+#It is very useful since you just need to change the variable 
+#and the program will automatically shows you the result.
