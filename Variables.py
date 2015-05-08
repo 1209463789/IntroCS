3297971d769d42c5d11ea6f70c6c8b8f3246a623
1209463789 <18ayuye@iaa.edu.ec>
2015-05-07 21:17:17 -0500

    Create Variables.py

---
+++ Variables.py
@@ -0,0 +1,27 @@
+name = "Arlen Yu Ye" #string
+age = 14 #interger
+gpa = 4.0 #floating point
+
+print name
+print gpa
+
+print 'My name is', name, '.I am', age, 'years old. My gpa is', gpa
+
+print 'In ten years, I will be', age + 10
+
+#Dog years
+ageInHumanyears = 3
+ageInDogYears = ageInHumanYears * 7
+
+print 'My dog is', ageInDogYears, 'years old in Dog Years'
+print 'But my dog is', ageInHumanYears, 'years old in Human Years.'
+
+#Order of operations
+#Parenthesis, Exponents, Multiplication, Division, Aaddition, Substraction
+myNumber = 5 * 4 + 10
+print myNumber
+print "Hello", 2
+
+import math
+root = math,sqrt(81)
+print root
