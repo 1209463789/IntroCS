99302008c441c5290793be3c5fcea87266b89fa8
1209463789 <18ayuye@iaa.edu.ec>
2015-05-07 21:14:47 -0500

    Create Practice.py

---
+++ Practice.py
@@ -0,0 +1,18 @@
+Name = "Arlen,"
+Last = "Yu,"
+Addres = "Via a Samborondon,"
+Email = "18ayuye@iaa.edu.ec,"
+Phone = "+593 979726851,"
+
+#Printing individualy for each variable
+print Name
+print Last
+print Addres
+print Email
+print Phone
+
+print #this creates a space between each print
+print #this creates a space between each print
+
+#Printing variables all together in one line
+print "My name is", Name, "and my last name is", Last, "I live in", Addres, "my email is", Email, "and my phone number is", Phone, "nice to meet you"
