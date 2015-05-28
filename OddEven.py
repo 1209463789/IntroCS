fbe3226ff7071cfd70f463cd472a723bc8628186
arlenyuye <18ayuye@iaa.edu.ec>
2015-05-28 12:57:01 -0200

    Create OddEven.py

---
+++ OddEven.py
@@ -0,0 +1,14 @@
+number = input("Please enter a number from 1 to 10:") 
+
+if number == 1:
+    print "The number is odd"
+elif number == 4:
+    print "The number is even"
+elif number == 2:
+    print "The number is even"
+elif number == 7:
+    print "The number is odd"
+elif number == 10:
+    print "The number is even"
+else:
+    print "The number is odd"
